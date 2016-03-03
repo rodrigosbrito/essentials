648dc4d8acd14d915fee19fc55139a79f6ab4472
rodrigosbrito <digao135>
2016-03-03 20:03:34 -0300

    teste

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lim_low, lim_up;
+            int a = 0, b = 1, c;
+
+            Console.WriteLine("Enter First Value: ");
+            lim_low = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Last Value: ");
+            lim_up = Convert.ToInt32(Console.ReadLine());
+
+            while (a < lim_up)
+            {
+                if (a > lim_low)
+                    Console.WriteLine(a);
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            Console.ReadLine();
+
+        }  
+    }
+}
